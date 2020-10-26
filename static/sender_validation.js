88de5aaf2da484b522e884f9c4eb6f754aac6eac
Wydzials <szymonstefan1999@gmail.com>
2020-10-26 21:52:11 +0100

    Upgrade sender sign up form validation

--- static/sender_validation.js
+++ static/sender_validation.js
@@ -1,50 +1,68 @@
 const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
 const password = document.getElementById("password");
 const confirm_password = document.getElementById("confirm_password");
+const login = document.getElementById("login");
 const form = document.getElementById('form');
+
+function setValid(form) {
+    form.classList.remove("is-invalid")
+    form.classList.add("is-valid");
+}
+
+function setInvalid(form) {
+    form.classList.remove("is-valid");
+    form.classList.add("is-invalid");
+}
+
 
 function check_password() {
     password1 = password.value;
     password2 = confirm_password.value;
 
-    passwords_not_match = document.getElementById("passwords_dont_match");
-    password_too_weak = document.getElementById("password_too_weak");
-
+    errors = 0;
     if (password1.length > 0 && password2.length > 0) {
-        if (password1 === password2) {
-            passwords_not_match.hidden = true;
-
-            if (password1.length < 8) {
-                password.className = "error";
-                confirm_password.className = "error";
-
-                password_too_weak.hidden = false;
-                return 1;
-            } else {
-                password.className = "ok";
-                confirm_password.className = "ok";
-
-                password_too_weak.hidden = true;
-                return 0;
-            }
+        if (password1.length >= 8) {
+            setValid(password);
         } else {
-            password.className = "error";
-            confirm_password.className = "error";
-            passwords_not_match.hidden = false;
-            password_too_weak.hidden = true;
-            return 1;
+            setInvalid(password);
+            errors++;
+        }
+
+        if (password1 == password2) {
+            setValid(confirm_password);
+        } else {
+            setInvalid(confirm_password);
+            errors++;
         }
     } else {
-        passwords_not_match.hidden = true;
-        password_too_weak.hidden = true;
+        errors++;
+    }
+    return errors;
+}
 
-        password.className = "ok";
-        confirm_password.className = "ok";
+function checkCase(ch) {
+    if (!isNaN(ch * 1)) {
+        return "numeric";
+    }
+    else if (ch == ch.toUpperCase()) {
+        return "upper";
+    }
+    else if (ch == ch.toLowerCase()) {
+        return "lower";
+    }
+}
+
+function checkName(name) {
+    if (name.length >= 2 && checkCase(name[0]) == "upper" && checkCase(name[1]) == "lower") {
+        return 0;
+    } else {
+        return 1;
     }
 }
 
 function submit(event) {
-    errors = check_password();
+    errors = check_password() + checkName(firstname.value) + checkName(lastname.value);
 
     if (errors > 0) {
         event.preventDefault();
@@ -52,15 +70,35 @@ function submit(event) {
     }
 }
 
-function attach_events() {
+function attachEvents() {
     form.onsubmit = submit;
 
-    firstname.addEventListener("change", function () {
-        console.log(firstname.value)
+    login.addEventListener("change", function () {
+        if (/^[a-z]+$/.test(login.value)) {
+            setValid(login);
+        } else {
+            setInvalid(login);
+        }
+    });
+
+    firstname.addEventListener("keyup", function () {
+        if (checkName(firstname.value) == 0) {
+            setValid(firstname);
+        } else {
+            setInvalid(firstname);
+        }
+    });
+
+    lastname.addEventListener("keyup", function () {
+        if (checkName(lastname.value) == 0) {
+            setValid(lastname);
+        } else {
+            setInvalid(lastname);
+        }
     });
 
     password.addEventListener("keyup", check_password);
     confirm_password.addEventListener("keyup", check_password);
 }
 
-attach_events()
+attachEvents()

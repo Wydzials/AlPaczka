4ab007d9a0d790148be86c087a015e31d8d41069
Wydzials <szymonstefan1999@gmail.com>
2020-11-10 19:15:56 +0100

    Add Redis database connection

--- web/app.py
+++ web/app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template
+from flask_session import Session
+
+from redis import Redis
+from os import getenv
+from dotenv import load_dotenv
+
+
+db = Redis(host="redis", port=6379, db=0)
+load_dotenv()
+SESSION_TYPE = "redis"
+SESSION_REDIS = db
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.secret_key=getenv("SECRET_KEY")
+ses = Session(app)
 
 @app.route("/")
 def index():
@@ -10,4 +24,4 @@ def sender_sign_up():
     return render_template("sender_sign_up.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", debug=True)

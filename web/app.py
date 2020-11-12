8f6316f018f625cd2fa5b46b623bc3a13c90acb7
Wydzials <szymonstefan1999@gmail.com>
2020-11-12 18:18:27 +0100

    Add sender login backend with Flask-Session

--- web/app.py
+++ web/app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_session import Session
 
 from redis import Redis
 from os import getenv
 from dotenv import load_dotenv
 from bcrypt import hashpw, gensalt, checkpw
+from datetime import datetime
 
 
 db = Redis(host="redis", port=6379, db=0)
 load_dotenv()
+
 SESSION_TYPE = "redis"
 SESSION_REDIS = db
+PERMANENT_SESSION_LIFETIME = 120
+SESSION_COOKIE_HTTPONLY = True
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key=getenv("SECRET_KEY")
@@ -37,7 +42,6 @@ def sender_register():
                     "address": "adresu",
                     "password": "hasła",
                     "password2": "potwierdzenia hasła"}
-
     fields = {}
     correct = True
 
@@ -68,9 +72,46 @@ def sender_register():
     hashed = hashpw(password, gensalt(5))
     db.hset(f"user:{fields['username']}", "password", hashed)
 
-    flash(f"Pomyślnie zarejestrowano nadawcę {fields['username']}.")
+    flash("Pomyślnie zarejestrowano konto nadawcy.")
     return redirect(url_for("index"))
 
+
+@app.route("/sender/login", methods=["GET", "POST"])
+def sender_login():
+    if request.method == "GET":
+        return render_template("sender_login.html")
+    
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username:
+        flash("Nazwa użytkownika nie może być pusta.")
+        return redirect(url_for("sender_login"))
+
+    if not password:
+        flash("Hasło nie może być puste.")
+        return redirect(url_for("sender_login"))
+
+    db_password = db.hget(f"user:{username}", "password")
+
+    if not db_password:
+        flash("Nie znaleziono użytkownika o podanej nazwie.")
+        return redirect(url_for("sender_login"))
+    
+    if not checkpw(password.encode(), db_password):
+        flash("Nieprawidłowe hasło.")
+        return redirect(url_for("sender_login"))
+    
+    session["username"] = username
+    session["logged-at"] = datetime.now()
+    flash("Zalogowano na konto nadawcy.")
+    return redirect(url_for("index"))
+
+@app.route("/sender/logout")
+def sender_logout():
+    session.clear()
+    flash("Wylogowano pomyślnie.")
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)

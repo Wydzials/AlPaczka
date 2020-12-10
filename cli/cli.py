d4e5584550e7842664a50f1907fa8a00566a9150
Wydzials <szymonstefan1999@gmail.com>
2020-12-10 21:14:20 +0100

    Add ability to create package from label

--- cli/cli.py
+++ cli/cli.py
@@ -14,9 +14,12 @@ statuses = {
     "delivered": "Dostarczona",
     "collected": "Odebrana"
 }
+flash_messages = ["(Sterowanie: strzałki + enter)"]
+
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def header():
     clear()
@@ -29,10 +32,10 @@ def header():
         print("Korzystanie z aplikacji wymaga posiadania ważnego tokenu.")
         exit()
 
-def info(message, message2="Naciśnij enter, aby kontynuować..."):
-    header()
-    print(message + "\n")
-    input(message2)
+    for message in flash_messages:
+        print("{: ^50}".format(message))
+    flash_messages.clear()
+
 
 def api(method, url, json=""):
     headers = {"Authorization": TOKEN}
@@ -51,50 +54,102 @@ def api(method, url, json=""):
         print("Błąd połącznia z API.")
         exit()
 
+
 def get_packages():
     sizes = {
-        1: "Mały", 
-        2: "Średni", 
+        1: "Mały",
+        2: "Średni",
         3: "Duży"
     }
 
     r = api("GET", "/courier/packages")
     data = r.json()
     packages = data.get("packages")
-    
+
     for package in packages:
         package["size"] = sizes[int(package["size"])]
         package["status"] = statuses[package["status"]]
 
     return packages
 
-def labels_list():
+
+def label_to_package(package_id):
+    question = {
+        "type": "list",
+        "name": "label",
+        "message": "Czy na pewno chcesz utworzyć paczkę?\n",
+        "choices": [
+            {
+                "name": "Tak",
+                "value": 1
+            },
+            {
+                "name": "Nie",
+                "value": 0
+            }
+        ],
+        "default": 1
+    }
+
+    choice = prompt(question).get("label")
+    if choice:
+        r = api(
+            "PATCH", f"/courier/packages/{package_id}", {"status": "in transit"})
+
+        if r.status_code == 200:
+            flash_messages.append("Utworzono paczkę z wybranej etykiety.")
+        else:
+            json = r.json()
+            if json and json.get("error_pl"):
+                flash_messages.append("Błąd: " + json.get("error_pl"))
+
+
+def list_menu(message, labels):
     packages_data = get_packages()
-    packages = [
-        "{: <10} {: <10} {: <10} {: <10}".format("Nadawca", "Odbiorca", "Skrytka", "Rozmiar")
+    row_format = "{: <10} {: <8} {: <12} {: <12} {: <12}"
+
+    choices = [
+        row_format.format("Skrytka", "Rozmiar", "Status",
+                          "Nadawca", "Odbiorca")
     ]
     for p in packages_data:
-        if p.get("status") == "Etykieta":
-            package = "{: <10} {: <10} {: <10} {: <10}".format(
-                p.get("sender"), 
-                p.get("recipient"), 
-                p.get("box_id"), 
-                p.get("size"), 
+        if (p.get("status") == "Etykieta") == labels:
+            name = row_format.format(
+                p.get("box_id"),
+                p.get("size"),
+                p.get("status"),
+                p.get("sender"),
+                p.get("recipient")
             )
-            packages.append(package)
-    
-    questions = {
+            choices.append(
+                {
+                    "name": name,
+                    "value": p.get("id")
+                }
+            )
+    choices.append(
+        {
+            "name": "[ Anuluj ]",
+            "value": "cancel"
+        }
+    )
+
+    question = {
         "type": "list",
-        "name": "package",
-        "message": "Wybierz etykietę do utworzenia paczki:\n",
+        "name": "value",
+        "message": message + "\n",
+        "choices": choices,
+        "default": len(choices) - 1
     }
-    questions["choices"] = packages
 
     header()
-    answer = prompt(questions)
-    while answer.get("package") == packages[0]:
+    choice = prompt(question).get("value")
+    while choice is None or choice == choices[0]:
         header()
-        answer = prompt(questions)
+        choice = prompt(question).get("value")
+
+    return choice
+
 
 def change_status(package_id):
     choices = []
@@ -102,7 +157,7 @@ def change_status(package_id):
         choices.append(
             {
                 "name": pl,
-                "value": en 
+                "value": en
             }
         )
 
@@ -118,53 +173,13 @@ def change_status(package_id):
     r = api("PATCH", f"/courier/packages/{package_id}", {"status": choice})
 
     if r.status_code == 200:
-        info("Zmieniono status paczki na: " + statuses[choice] + ".")
+        flash_messages.append(
+            "Zmieniono status paczki na: " + statuses[choice] + ".")
     else:
         json = r.json()
         if json and json.get("error_pl"):
-            info("Błąd: " + json.get("error_pl"))
+            flash_messages.append("Błąd: " + json.get("error_pl"))
 
-
-def packages_list():
-    packages_data = get_packages()
-    row_format = "{: <12} {: <12} {: <10} {: <8} {: <10} {: <20}"
-
-    choices = [
-        row_format.format("Nadawca", "Odbiorca", "Skrytka", "Rozmiar", "Status", "ID")
-    ]
-    for p in packages_data:
-        if p.get("status") != "Etykieta":
-            name = row_format.format(
-                p.get("sender"), 
-                p.get("recipient"), 
-                p.get("box_id"), 
-                p.get("size"), 
-                p.get("status"),
-                p.get("id")
-            )
-            choices.append(
-                {
-                    "name": name,
-                    "value": p.get("id")
-                }
-            )
-    choices.append("[ Anuluj ]")
-
-    question = {
-        "type": "list",
-        "name": "package",
-        "message": "Wybierz paczkę do zmiany statusu:\n",
-        "choices": choices
-    }
-
-    header()
-    choice = prompt(question).get("package")
-    while choice == choices[0]:
-        header()
-        choice = prompt(question).get("package")
-
-    if choice != "[ Anuluj ]":
-        change_status(choice)
 
 def menu():
     header()
@@ -195,10 +210,18 @@ def menu():
     ]
     answers = prompt(questions)
     choice = answers.get("choice")
+
     if choice == 0:
-        labels_list()
-    if choice == 1:
-        packages_list()
+        choice = list_menu(
+            "Wybierz etykietę do utworzenia paczki:", labels=True)
+        if choice != "cancel":
+            label_to_package(choice)
+
+    elif choice == 1:
+        choice = list_menu("Wybierz paczkę do zmiany statusu:", labels=False)
+        if choice != "cancel":
+            change_status(choice)
+
     elif choice == 3:
         clear()
         print("Do zobaczenia!")
@@ -214,4 +237,3 @@ exp = datetime.utcfromtimestamp(authorization.get("exp"))
 
 while True:
     menu()
-    
